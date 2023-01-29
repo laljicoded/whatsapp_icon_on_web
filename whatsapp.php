800bed0877f631bab2d5a9621d5cbefb2dc4d93b
laljicoded <lalji5029@gmail.com>
2023-01-30 03:24:11 +0530

    whatsapp icon on web

---
+++ whatsapp.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <style>
+      .whatsapp_float {
+        position: fixed;
+        bottom: 40px;
+        right: 20px;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="whatsapp_float">
+      <a href=" https://wa.me/918700253938"
+        ><img
+          src="/WhatsApp_icon.png"
+          alt=""
+          width="50px"
+          class="whatsapp_float_btn"
+      /></a>
+    </div>
+  </body>
+</html>
